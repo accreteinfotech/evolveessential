9fe66af109915566538a7a9d911fa2ffd76516b4
roshan8888 <roshan.u.desai@gmail.com>
2020-10-30 14:40:29 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <!--====== Title ======-->
+     <title>Evolve | Coming Soon</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+
+    <!-- CSS
+    ============================================ -->
+
+    <!--===== Vendor CSS (Bootstrap & Icon Font) =====-->
+
+    <!-- <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/default.css"> -->
+
+
+    <!--===== Plugins CSS (All Plugins Files) =====-->
+    <!-- <link rel="stylesheet" href="assets/css/plugins/animate.css">
+    <link rel="stylesheet" href="assets/css/plugins/slick.css">
+    <link rel="stylesheet" href="assets/css/plugins/component.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/plugins/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/select2.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/photoswipe.css">
+    <link rel="stylesheet" href="assets/css/plugins/photoswipe-default-skin.css"> -->
+
+
+    <!--====== Main Style CSS ======-->
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+    <link rel="stylesheet" href="assets/css/vendor/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> 
+
+</head>
+
+<body>
+
+
+    <!--====== preloader Start ======-->
+
+    <div class="preloader">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+
+    <!--====== preloader Ends ======-->
+    
+    <!--====== Header Start ======-->
+       <header class="header-area">
+
+        <div class="header-navbar header-navbar-white">
+            <div class="container-fluid custom-container">
+                <div class="header-wrapper d-flex justify-content-between align-items-center">
+
+                    <div class="header-logo">
+                        <a>
+                            <img src="assets/images/logo-white.png" alt="Logo">
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </header>
+	 <!--====== Off Canvas Sidebar Start ======-->
+
+    <!--====== Off Canvas Sidebar Ends ======-->
+
+    <!--====== Search Start ======-->
+
+   
+
+    <!--====== Search Ends ======-->
+
+    <!--====== Off Canvas Cart Start ======-->
+
+    <!--====== Off Canvas Cart Ends ======-->
+    <!--====== Header Ends ======-->
+
+    <!--====== Search Start ======-->
+	<!--====== Search Ends ======-->
+
+    <!--====== Off Canvas Cart Start ======-->
+
+    <!--====== Off Canvas Cart Ends ======-->
+
+    <!--====== Off Canvas Sidebar Start ======-->
+
+    <!--====== Off Canvas Sidebar Ends ======-->
+
+    <!--====== Coming Soon Start ======-->
+
+    <section class="coming-soon-page bg_cover d-flex align-items-center" style="background-image: url(assets/images/coming-bg.jpg);">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6 col-lg-7 col-md-9">
+                    <div class="coming-soon-content text-center">
+                        <h4 class="sub-title" style="font-size:40px">Evolution</h4>
+                        <h2 class="main-title">Coming Soon</h2>
+
+                        <div class="coming-soon-countdown">
+                            <!--<div data-countdown="2020/11/20"></div>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--====== Coming Soon Ends ======-->
+
+    <!--====== Footer Start ======-->
+
+    <!--====== Footer Ends ======-->
+
+    <!--====== BACK TOP TOP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="fal fa-chevron-up"></i></a>
+
+    <!--====== BACK TOP TOP PART ENDS ======-->
+
+    <!--====== Product Quick View Start ======-->
+
+    <!--====== Product Quick View Ends ======-->
+
+    <!--====== Start ======-->
+
+    <!--
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+-->
+
+    <!--====== Ends ======-->
+
+    <!--====== Overlay Start ======-->
+
+    <div class="overlay"></div>
+
+    <!--====== Overlay Ends ======-->
+
+
+
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+
+    <!--====== All Plugins js ======-->
+    <!-- <script src="assets/js/plugins/jquery-ui.min.js"></script>
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/slick.min.js"></script>
+    <script src="assets/js/plugins/modernizr.custom.js"></script>
+    <script src="assets/js/plugins/jquery.dlmenu.js"></script>
+    <script src="assets/js/plugins/jquery.paroller.min.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/select2.min.js"></script>
+    <script src="assets/js/plugins/photoswipe.min.js"></script>
+    <script src="assets/js/plugins/photoswipe-ui-default.min.js"></script>
+    <script src="assets/js/plugins/jquery.elevateZoom.min.js"></script>
+    <script src="assets/js/plugins/masonry.pkgd.min.js"></script>
+    <script src="assets/js/plugins/jquery.appear.min.js"></script>
+    <script src="assets/js/plugins/jquery.sticky-sidebar.min.js"></script>
+    <script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/plugins/ajax-contact.js"></script> -->
+
+    <script src="assets/js/plugins/parallax.min.js"></script>
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+    <script src="assets/js/plugins.min.js"></script>
+
+
+    <!--====== Main Activation  js ======-->
+    <script src="assets/js/main.js"></script>
+
+    <!--====== Google Map js ======-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQ5y0EF8dE6qwc03FcbXHJfXr4vEa7z54"></script>
+    <script src="assets/js/map-script.js"></script>
+
+</body>
+
+</html>
